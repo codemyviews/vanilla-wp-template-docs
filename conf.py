0f9b32aa3ec46cd95e9880092bf0f65b3ec81826
Buzdyk Gregory <buzdyk.gregory@gmail.com>
2016-10-29 04:23:52 +0300

    remove the githubpages ext

--- conf.py
+++ conf.py
@@ -33,8 +33,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.intersphinx',
-    'sphinx.ext.todo',
-    'sphinx.ext.githubpages',
+    'sphinx.ext.todo'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
